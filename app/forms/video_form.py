0268acdedf2570f495619fe45c720d9b1d076e10
Spencer Wilfahrt <spencerwilf@yahoo.com>
2023-04-21 10:08:16 -0700

    untested aws setup

--- app/forms/video_form.py
+++ app/forms/video_form.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Length, ValidationError, URL
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from ..api.aws_helpers import ALLOWED_EXTENSIONS
 
 class VideoForm(FlaskForm):
     title = StringField('Title')
-    description = StringField('Title')
-    thumbnail = StringField('Title')
-    title = StringField('Title')
+    url = FileField('Image File', validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+    description = StringField('Description')
+    thumbnail = StringField('Thumbnail')
+    category = StringField('Category')
