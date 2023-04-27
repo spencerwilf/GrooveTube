36446f1bf48f0335f52d2c45d365529929b5cd25
Spencer Wilfahrt <spencerwilf@yahoo.com>
2023-04-27 02:00:20 -0700

    LETS GOOOO

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SplashPage.css'
-import logo from '../../media/disco-only.png'
+import logo from '../../media/GrooveTube.png'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import me from '../../media/me.jpg'
-import ball from '../../media/ball.png'
+import ball from '../../media/discoball.png'
+import September from '../../media/September2.mp3'
 
 const SplashPage = () => {
-    
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const togglePlay = () => {
+        const audio = document.getElementById('audio');
+        if (isPlaying) {
+            audio.pause();
+        } else {
+            audio.play();
+        }
+        setIsPlaying(!isPlaying);
+    };
+
   return (
     <div className='splash-all-wrapper'>
+          <button className='audio-play-button' onClick={togglePlay}>{isPlaying ? "Stop the party!" : 'Party Button'}</button>
+          <audio id="audio" src={September}/>
         <section className='splash-top-section'>
-          <div className='splash-page-top-animation'>
-            
+              <Link to='/'><span className='header-logo-splash-span'><img className='header-nav-logo-render' src={logo} /></span></Link>
+          <div className={isPlaying ? 'splash-page-top-animation party' : 'splash-page-top-animation regular'}>
+            {!isPlaying && <img className='inactive-party-ball' src={ball}/>}
           </div>
-          <div className='splash-logo-and-header'>
-          <span className='welcome-to-groovetube'>Welcome to GrooveTube!</span>
-          <img className='splash-logo' src={logo}/>
-        </div>
+
+              <Link to='/'><button className='go-to-app-button'>Take me to the app!</button></Link>
+
         </section>
         <section className='splash-second-section'>
               <a target='_blank' href='https://www.linkedin.com/in/spencer-wilfahrt-1a4604156'>
@@ -31,7 +45,7 @@ const SplashPage = () => {
                   </div>
             </div>
               </a>
-            <Link to='/'><button className='go-to-app-button'>GO TO THE APP</button></Link>
+            
         </section>
     </div>
   )
